0eebdabecd1176bc89c8c62b9cd0571dcdacf950
arpanrau <Arpan.Rau@gmail.com>
2015-11-14 21:23:03 -0500

    added correct headbar links

--- flasktest/app/views.py
+++ flasktest/app/views.py
@@ -3,7 +3,19 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    # fake user
+    # main page
     return render_template('index.html',
                            title='Home')
+@app.route('/about')
+def rankings():
+    #Rankings page
+    return render_template('about.html',
+                           title ='Home')
+
+@app.route('/about')
+def about():
+    #about us page
+    return render_template('about.html',
+                           title ='Home')
