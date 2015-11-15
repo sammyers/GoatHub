f26680d4bb22bb0b33842b2de5dcdc2431cb9106
arpanrau <Arpan.Rau@gmail.com>
2015-11-14 20:46:23 -0500

    This thing is obese

---
+++ flasktest/flask/Scripts/migrate-repository-script.py
@@ -0,0 +1,10 @@
+#!c:\users\arau\documents\flasktest\flask\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'sqlalchemy-migrate==0.10.0','console_scripts','migrate-repository'
+__requires__ = 'sqlalchemy-migrate==0.10.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('sqlalchemy-migrate==0.10.0', 'console_scripts', 'migrate-repository')()
+    )
