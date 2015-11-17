2c4f13b1c25eb04a8b79339516585ef5e8a0a386
Danny Wolf <wolf@students.olin.edu>
2015-11-17 12:57:51 -0500

    Remove secrets (use environment variables to provide). Add *.pyc to gitignore. Remove current pyc files. Remove backup index file. Minor readme changes.

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://goat:sam4core@goathub.cjobi9tevpql.us-west-2.rds.amazonaws.com:3306/goathub'
+
+
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -9,3 +13,4 @@ SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '6qNFknUx3zp8BXSCf16P++rzGMu+L8SmjPMJE1c3'
+
